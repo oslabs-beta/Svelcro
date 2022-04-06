8f799319ee02235810041de3f8d97d3653598eef
Yanming Yu <yuyanming1994@hotmail.com>
2022-04-06 12:17:18 -0700

    first commit boiler plate
    Co-authored-by: Rankin Draa <rankindraa@gmail.com>
    Co-authored-by: Zack Daniels <zackdaniels2522@gmail.com>
    Co-authored-by: Sara KiviKas <sarakivikas@gmail.com>

--- devtools.js
+++ devtools.js
@@ -1,2 +1,2 @@
-chrome.devtools.panels.create(title, htmlFile, callback);
-chrome.devtools.panels.create('Svelcro', 'panel.html');
+
+chrome.devtools.panels.create('Svelcro', 'callback','panel.html');

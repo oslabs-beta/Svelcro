fa2fa01ff70c088c77344c50c8e5122ceb804a1b
Yanming Yu <yuyanming1994@hotmail.com>
2022-04-06 17:44:45 -0700

    navbar boilerplate
    Co-authored-by: Rankin Draa <rankindraa@gmail.com>
    Co-authored-by: Zack Daniels <zackdaniels2522@gmail.com>
    Co-authored-by: Sara KiviKas <sarakivikas@gmail.com>

---
+++ dev-app/src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+	// COMPONENT IMPORTS
+	import Nav from './components/nav.svelte';
+	import Router from 'svelte-spa-router';
+	import ComponentDisplay from './componentDisplay.svelte'
+  import ProfilerDisplay from './profilerDisplay.svelte'
+  import TimeMachineDisplay from './timeMachineDisplay.svelte'
+	export let name;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<Nav/>
+</main>
+
+<style>
+
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+	
+</style>
---
+++ dev-app/src/components/nav.svelte
@@ -0,0 +1,62 @@
+<script>
+  // COMPONENT IMPORTS
+  import ComponentDisplay from './componentDisplay.svelte'
+  import ProfilerDisplay from './profilerDisplay.svelte'
+  import TimeMachineDisplay from './timeMachineDisplay.svelte'
+  import Router, {location} from 'svelte-spa-router'
+
+  import Hidden from './Hidden.svelte';
+
+  
+    const navItems = [
+        {label: "Component"},
+        {label: "Profiler"},
+        {label: "Time Machine"},
+    ];
+
+    ///create onclick function
+     const onclick = () => {
+         console.log('user clicked')
+
+     }
+
+</script>
+
+
+<nav>
+  <a href="/#/componentDisplay">Components</a>
+  <a href="/#/profiler">Profiler</a>
+  <a href="/#/timeMachineDisplay">TimeMachine</a>
+    <!-- <ul class ='navbar'>
+        <li>
+            <button on:click={ComponentDisplay.show}>Component</button>
+            <button on:click={ProfilerDisplay.show}>Profiler</button>
+            <button on:click={TimeMachineDisplay.show}>Time Machine</button>
+        </li>
+    </ul> -->
+    
+    
+</nav>
+
+<Router routes={{
+  '/componentDisplay': ComponentDisplay,
+  '/profiler': ProfilerDisplay,
+  '/timeMachineDisplay': TimeMachineDisplay
+}}/>
+
+<style>
+
+  nav {
+    background-color: rgba(122, 122, 122, 0.8);
+    font-family: "Helvetica Neue", "Helvetica", "Arial", sans-serif;
+    height: 45px;
+  }
+
+  ul{
+    display: flex;
+    list-style: none;
+    align-items: stretch;
+    justify-content: space-evenly;
+  }
+
+</style>

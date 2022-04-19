2077440ab517dd82eaef06d73f9009d601d476e9
Sara Kivikas <sarakivikas@saras-mbp.lan>
2022-04-18 19:58:02 -0700

    committing after working on background and content scripts Co-authored-by: Rankin Draa <rankindraa@gmail.com> Co-authored-by: Zack Daniels <zackdaniels2522@gmail.com> Co-authored-by: Yanming Yu <yuyanming1994@hotmail.com>

--- background.js
+++ background.js
@@ -1,1 +1,29 @@
-// may be used in the future
+chrome.runtime.onInstalled.addListener(()=>{
+    console.log("This is coming from the background.js");
+
+    //right click Svelcro devtools context menu option
+    chrome.contextMenus.create({
+        "id" : "Svelcro",
+        "title" : "Svelcro DevTools",
+        "contexts" : ["all"]
+    })
+
+    
+    
+    chrome.runtime.onMessage.addListener((msg, sender, response)=> {
+        // console.log('hello', msg);
+
+        if(msg.body){
+            
+            const { body } = msg;
+            console.log('recieving at background! Coming from ', body);
+            console.log('next step?')
+            response({
+                body: "background content"
+            });
+
+        }
+        return true;
+    })
+
+})
--- dev-app/src/components/profiler/ProfilerStats.svelte
+++ dev-app/src/components/profiler/ProfilerStats.svelte
@@ -1,7 +1,13 @@
 <script>
 // COMPONENT IMPORTS
 
-
+chrome.runtime.onMessage.addListener((msg, sender, response) => {
+    if (msg.body) {
+      const { body } = msg;
+      console.log("recieving at Dev Tools! Coming from ", body);
+    }
+    return true;
+  });
 </script>
 
 <div id="profiler-Stats">

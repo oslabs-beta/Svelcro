89e237c304d62f82ccf2d3810cad07dd6535dc0c
Yanming Yu <69600884+jimmyjameswong@users.noreply.github.com>
2022-04-21 10:57:52 -0700

    Injected scripted sends data to dev tools
    Co-authored-by: Rankin Draa <rankindraa@gmail.com>
    Co-authored-by: Zack Daniels <zackdaniels2522@gmail.com>
    Co-authored-by: Sara KiviKas <sarakivikas@gmail.com>

--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,3 @@
-
 // window.tag = document.createElement('script');
 // const root = document.getElementById('root');
 // while (root.children.length) {
@@ -31,10 +30,25 @@
     
   }
   getAllComps();
+  // MING TEST
   window.document.addEventListener("click", function(){
-    console.log("PAGE LOADED")
+    // console.log("chrome.runtime.id: ", chrome.runtime.id);
+    // let editorExtensionId = 'bdbhodpgbllbabnnafknafigdemaahdd';
+
+    // chrome.runtime.sendMessage(editorExtensionId, { body: "contentScript" });
+
+    // window.postMessage({
+    //   body: "contentScript"
+    // })
+    // var channel = new MessageChannel();
+
+    // console.log('channel object port 1: ', channel.port1);
+
+    // channel.port1.postMessage({body:"contentScript"});
     
   }, true)
+
+  
 
 
   // window.document.addEventListener("SvelteDOMSetData", (e) => {
@@ -42,39 +56,53 @@
   // })
 
   // object to hold render counts by component
-  const compCounts = {};
+  // const compCounts = {};
   let nextId = 1;
+  // MING TEST
+  // Proxy object that trigger function when property values are changed
+  var compCounts = new Proxy({}, {
+    set: function (target, key, value) {
+        console.log(`${key} set to ${value}`);
+        target[key] = value;
+
+        // Send render count records to dev Tools
+        let editorExtensionId = 'bdbhodpgbllbabnnafknafigdemaahdd';
+        chrome.runtime.sendMessage(editorExtensionId, { body: 'UPDATE_RENDER', data: JSON.stringify(target) });
+        return true;
+    }
+  });
   // add all Svelte components to array
-    window.document.addEventListener('SvelteRegisterComponent', (e) => {
-      
-      let isFirstAfterUpdate = true;
-      
-      const { component, tagName } = e.detail;
+  window.document.addEventListener('SvelteRegisterComponent', (e) => {
+    
+    let isFirstAfterUpdate = true;
+    
+    const { component, tagName } = e.detail;
 
-      component.$$['id'] = tagName + nextId;
-      nextId++;
-      const curId = component.$$.id;
-      compCounts[curId] = 1;
-      components.push(e.detail.component)
+    component.$$['id'] = tagName + nextId;
+    nextId++;
+    const curId = component.$$.id;
+    compCounts[curId] = 1;
+    components.push(e.detail.component)
 
-      console.log('is the id there?', component.$$)
-      console.log('components', components)
-      // console.log('event', e)
+    // console.log('is the id there?', component.$$)
+    // console.log('components', components)
+    console.log('current components:', component)
+    // console.log('event', e)
 
-      
+    
 
-    console.log('VERY FIRST`:', window.performance.now())
+    // console.log('VERY FIRST`:', window.performance.now())
     
     component.$$.before_update.push(() => {
       //////
       // const curId = component.$$.ctx[0].id;
       // console.log( tagName, ' ' ,component.$$.ctx[0].id, 'beforeUpdate');
       // console.log(Date.now())
-      console.log('BEFORE IS:', window.performance.now())
+      // console.log('BEFORE IS:', window.performance.now())
       // if (compCounts.hasOwnProperty(curId)) compCounts[curId] += 1;
       // else compCounts[curId] = 1;
 
-      console.log("compCounts", compCounts);
+      // console.log("compCounts", compCounts);
     });
 
     component.$$.after_update.push(() => {
@@ -86,7 +114,7 @@
       //  if (compCounts.hasOwnProperty(curId)) compCounts[curId] += 1;
       // else compCounts[curId] = 1;
       compCounts[curId] += 1;
-     
+      
       
     });
 
@@ -95,7 +123,48 @@
     // console.log('performance:', performanceEntries)
     // performanceEntries.forEach( (performanceEntry, i, entries) => {
     //   console.log("The time to " + performanceEntry.name + " was " + performanceEntry.startTime + " milliseconds.")})
-})
+
+
+
+    // MING TEST
+    
+    // // current_component is the component being initialized; at the time 
+    // // our event is called, it has already been reverted from the component 
+    // // that triggered the event to its parent component
+    // const parentComponent = current_component;
+
+    // let node = nodes.get(component);
+    // if (!node) {
+    //   node = { children: [] };
+    //   nodes.set(component, node);
+    // }
+    // Object.assign(node, e.detail);
+
+    // // children creation is completed before their parent component creation
+    // // is completed (necessarilly, since the parent needs to create all its
+    // // children to complete itself); that means that the dev event we're using
+    // // is fired first for children... and so we may have to add a node for the
+    // // parent from the (first created) child
+    // let parent = nodes.get(parentComponent);
+    // if (!parent) {
+    //   parent = { children: [] };
+    //   nodes.set(parentComponent, parent);
+    // }
+    // parent.children.push(node);
+
+    // // we're done mutating our tree, let the world know
+    // notify();
+
+    // // abusing a little bit more of Svelte private API...
+    // component.$$.on_destroy.push(() => {
+    //   const index = parent.children.indexOf(node);
+    //   if (index >= 0) {
+    //     parent.children.splice(index, 1);
+    //     notify();
+    //   }
+    // });
+
+  })
 
 
   
@@ -142,25 +211,8 @@
     // root.addEventListener('SvelteDOMRemoveAttribute', (e) => sendMessages(parseEvent(e.detail)));
   };
 
-  // setTimeout(() => setupListeners(window.document));
 
-
-
-  // window.addEventListener(
-  //   "message",
-  //   (messageEvent) => {
-  //     if (messageEvent.data.body === 'TIME_TRAVEL') {
-  //       const i = messageEvent.data.ctxIndex;
-  //       lastIndex = i;
-  //       if (cacheState[i]) {
-  //         cacheState[i].forEach((componentState) => {
-  //           componentState[0].$inject_state(componentState[1])
-  //         })
-  //       }
-  //     }
-  //   },
-  //   false
-  // );
-  })();
+ 
+})();
   
 
